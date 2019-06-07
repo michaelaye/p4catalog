31cdb81745889ea48bcafe2120ce1438d7ee71d3
K.-Michael Aye <kmichael.aye@gmail.com>
2019-06-06 19:46:23 -0600

    changing data access path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p $CONDA_PREFIX/share/intake/p4data
-cp $RECIPE_DIR/p4data.yaml $CONDA_PREFIX/share/intake/
-cp $RECIPE_DIR/p4data_sub.yaml $CONDA_PREFIX/share/intake/p4data
+mkdir -p $CONDA_PREFIX/share/intake/planet4
+cp $RECIPE_DIR/planet4.yaml $CONDA_PREFIX/share/intake/
+cp $RECIPE_DIR/planet4_files.yaml $CONDA_PREFIX/share/intake/planet4
